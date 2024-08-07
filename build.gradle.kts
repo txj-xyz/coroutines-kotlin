225b1e3992d2f034cf52c1c9f4219d3876e6fb25
txj-xyz <7461476+txj-xyz@users.noreply.github.com>
2024-08-07 11:59:41 -0600

    feat: learning coroutines/threading

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0-RC")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
